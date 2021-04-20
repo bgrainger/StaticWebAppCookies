03f02630e915af6d1345671cc7900b225f911432
Bradley Grainger <bgrainger@gmail.com>
2021-04-19 19:30:54 -0700

    Add cookie test API.

--- api/Test.cs
+++ api/Test.cs
@@ -1,12 +1,9 @@
-using System;
-using System.IO;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
-using Newtonsoft.Json;
 
 namespace api
 {
@@ -17,19 +14,16 @@ namespace api
             [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
             ILogger log)
         {
-            log.LogInformation("C# HTTP trigger function processed a request.");
+            var cookie = req.Cookies["test"];
+            var message = $"Request Cookie = {cookie}.";
 
-            string name = req.Query["name"];
-
-            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-            dynamic data = JsonConvert.DeserializeObject(requestBody);
-            name = name ?? data?.name;
-
-            string responseMessage = string.IsNullOrEmpty(name)
-                ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
-                : $"Hello, {name}. This HTTP triggered function executed successfully.";
-
-            return new OkObjectResult(responseMessage);
+            var setcookie = req.Query["setcookie"];
+            if (setcookie.Count != 0)
+            {
+                req.HttpContext.Response.Cookies.Append("test", setcookie[0]);
+                message += $" Setting cookie to {setcookie[0]}.";
+            }
+            return new OkObjectResult(message);
         }
     }
 }
